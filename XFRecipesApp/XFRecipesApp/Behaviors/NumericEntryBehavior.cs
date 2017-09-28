20c64696c8bf5ba3fc8f4bbbfca9f8f1480f4b2a
apolo96 <andresfpvclap@gmail.com>
2017-09-28 16:25:29 -0500

    Add behavior to Entry control

---
+++ XFRecipesApp/XFRecipesApp/Behaviors/NumericEntryBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFRecipesApp.Behaviors
+{
+    class NumericEntryBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += textChangedHandler;
+        }
+
+        private void textChangedHandler(object sender, TextChangedEventArgs e)
+        {
+            double _out;
+            if (string.IsNullOrEmpty(e.NewTextValue)) return;
+            if (!double.TryParse(e.NewTextValue, out _out))
+                ((Entry)sender).Text = e.OldTextValue;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= textChangedHandler;
+        }
+    }
+}
